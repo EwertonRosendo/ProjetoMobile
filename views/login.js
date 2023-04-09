c92acc18d74ea170d8e329881dc2a6e6b24d836e
Rosendo <ewerton.rosendo15@gmail.com>
2023-04-08 21:54:07 -0300

    3 paginas prontas

--- views/login.js
+++ views/login.js
@@ -1,59 +1,139 @@
-import React, { useState } from 'react';
-import{ Text, TextInput, View, StyleSheet, Button, TouchableOpacity } from'react-native';
+import React, { useState, Fragment } from 'react';
+import{ Text, TextInput, View, StyleSheet, Button, TouchableOpacity, SafeAreaView ,ScrollView } from'react-native';
 import{ Entypo } from'@expo/vector-icons';
 import{ FontAwesome5 } from'@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+
+
+
 export default function App() {
 
   const[click, setClick] =useState(true)
+  const[textBotao, setTextBotao] = useState('Cadastro')
+  const[mode, setMode] = useState(true)
+
+  const [background, setBackground ] = useState('#e8e8e8')
+  const [backgroundIcon, setBackgroundIcon ] = useState('#474747')
+  const [container_color, setContainerColor ] = useState('#e8e8e8')
+  const [logo, setLogo ] = useState('green')
+  const [backgroundInput, setBackgroundInput ] = useState('#e8e8e8')
+  const [iconsInput, setIconsInput ] = useState('#e8e8e8')
+  const [label, setLabel ] = useState('#e8e8e8')
+  const [backgroundBotao, setBackgroundBotao ] = useState('#e8e8e8')
+  const [textoBotao, setTextoBotao ] = useState('#e8e8e8')
+
+  const [alerta, setAlerta] = useState('Você está usando um Email invalido!')
   const[email, setEmail] =useState('');
   const[senha, setSenha] =useState('');
-  const[textBotao, setTextBotao] =useState('Login')
 
-  function mudarBool(){
-    setClick(!click)
-    if(click==true){
-      setTextBotao("Login")
+  function mudarMode(){
+    
+    setMode(!mode)
+    
+    if(mode){
+      cor = 'black'
     }else{
-      setSenha('')
-      setEmail('')
-      setTextBotao("Deslogar")}
+      cor = 'red'
     }
+  }
+
+/*
+  function mudarMode(){
+    setMode(!mode)
+    if(mode){
+      setBackground('#e8e8e8')
+      setBackgroundIcon('#e8e8e8')
+      setContainerColor('#e8e8e8')
+      setBackgroundInput('#e8e8e8')
+      setIconsInput('#e8e8e8')
+      setLabel('#e8e8e8')
+      setBackgroundBotao('#e8e8e8')
+      setTextoBotao('#e8e8e8')
+    }
+    else{
+      setBackground('#474747')
+      setBackgroundIcon('#474747')
+      setContainerColor('#474747')
+      setBackgroundInput('#474747')
+      setIconsInput('#474747')
+      setLabel('#474747')
+      setBackgroundBotao('#474747')
+      setTextoBotao('#474747')
+
+    }
+  }*/
+  function validEmail(){
+    let pos = email.indexOf('@')
+    if(pos==-1){
+      setAlerta('Você está usando um Email invalido!')
+      return false
+    } else{
+      setAlerta('')
+      return true
+    }
+  }
     
     return(
-      <View style={styles.container}>
+      <Fragment>
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#E7E7E7' }}>
       
-        <Text style={styles.logo}>Nilson</Text>
+        <View style={styles.mode}>
+        
+          <TouchableOpacity style={styles.icon} onPress={mudarMode}>
+          {
+            mode ?
+            <Feather name="sun" size={24} color="white" /> //claro
+            
+            :
+            <FontAwesome5 name="cloud-moon" size={24} color="white" /> //escuro
 
-        <View style={styles.backgroundInput}>
-          <Entypo name="mail"size={24}color="black"/>
-          <TextInput style={styles.input}placeholder="Email:"value={email}onChangeText={setEmail}/>
+          }
+          
+          </TouchableOpacity>
+            
+          
+          <View style={styles.container}>
+          
+            <Text style={styles.logo}>Barbearia</Text>
+
+            <View style={styles.backgroundInput}>
+              <Entypo name="mail"size={24}color="black"/>
+              <TextInput style={styles.input}placeholder="Email:"value={email} onChangeText={setEmail}/>
+            </View>
+
+            <View style={styles.backgroundInput}>
+              <FontAwesome5 name="key"size={24}color="black"/>
+              <TextInput style={styles.input}placeholder="Senha:"value={senha} onChangeText={setSenha}/>
+            </View>
+            <View style={styles.forgot}>
+              <Text style={styles.textEmail}>{alerta}</Text>
+            </View>
+            
+            <TouchableOpacity style={styles.forgot}>
+              <Text style={styles.textForgot}>Não possui uma conta?</Text>
+            </TouchableOpacity>
+            
+            <TouchableOpacity style={styles.cadastro} onPress={validEmail}>
+              <Text style={styles.cadastroText}>Sign In</Text>
+            </TouchableOpacity>
+          </View>
+            
         </View>
-
-      <View style={styles.backgroundInput}>
-        <FontAwesome5 name="key"size={24}color="black"/>
-        <TextInput style={styles.input}placeholder="Senha:"value={senha}onChangeText={setSenha}/>
-      </View>
-
-      <TouchableOpacity style={styles.forgot}>
-        <Text style={styles.textForgot}>Esqueceu sua senha?</Text>
-      </TouchableOpacity>
-      
-        <TouchableOpacity style={styles.login}>
-          <Text style={styles.loginText}>Sign In</Text>
-        </TouchableOpacity>
-      </View>
+        </SafeAreaView>
+      </Fragment>
       );
 }
 const styles=StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor:'#fff',
+    //backgroundColor:'#fff',
     alignItems:'center',
-    justifyContent:'center'
+    marginTop:'20%'
 },
 input:{
   marginLeft:5,
-   padding:10
+   padding:10,
+   width:'90%',
 },
 backgroundInput:{
   alignItems:'center',
@@ -71,8 +151,8 @@ logo:{
   margin:40,
   fontWeight: 'bold'
 },
-login:{
-  backgroundColor:'blue',
+cadastro:{
+  backgroundColor:'#109DFA',
   width:'80%',
   height:50,
   justifyContent:'center',
@@ -80,7 +160,7 @@ login:{
   borderRadius:10,
   marginTop:20,
 },
-loginText:{
+cadastroText:{
   fontSize:30,
   color:'white',
   fontWeight: 'bold'
@@ -91,6 +171,26 @@ forgot:{
   marginRight:20, 
 },
 textForgot:{
-  fontWeight: 'bold'
-}
+  fontWeight: 'bold',
+  color:'#109DFA'
+},
+mode:{
+  flex:1,
+  marginTop:40,
+  backgroundColor:'#E7E7E7',
+  
+},
+icon:{
+  margin:20,
+  width:40,
+  height:40,
+  backgroundColor:'black',
+  alignItems:'center',
+  justifyContent:'center',
+  borderRadius:10, 
+},
+textEmail:{
+  fontWeight: 'bold',
+  color:'#BB5959'
+},
 });
